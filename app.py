3476acc85cede66dbdd8b96f9e4b8687804f42f2
Fabiano Castello <fabianocastello@gmail.com>
2021-09-05 09:16:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,16 @@ import socket
 st.title('My first app @ heroku 4')
 st.write("socket:", socket.gethostname())
 
+t1 = os.environ['teste']
+t2 = os.environ['another']
+t3 = os.environ['another2']
+t4 = os.environ['another3']
+
+st.write(t1)
+st.write(t2)
+st.write(t3)
+st.write(t4)
+
 st.write("Here's our first attempt at using data to create a table:")
 st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
@@ -20,7 +30,7 @@ st.write(pd.DataFrame({
 latest_iteration = st.empty()
 bar = st.progress(0)
 
-for i in range(100):
+for i in range(10):
   # Update the progress bar with each iteration.
   latest_iteration.text(f'Iteration {i+1}')
   bar.progress(i + 1)

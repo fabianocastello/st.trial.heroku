66f9b0e579a99aecc222f14c4210f106a9338243
Fabiano Castello <fabianocastello@gmail.com>
2021-09-05 12:15:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ from flask import request
 from flask import jsonify
 
 @app.route("/get_my_ip", methods=["GET"])
- def get_my_ip():
+def get_my_ip():
      return jsonify({'ip': request.remote_addr}), 200
 
 # x = 'teste'#get_my_ip()
